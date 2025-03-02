08c6136d4af234d8ddc7fc9869bd59ae659233f0
Gustavo Almao <gustavoalmao.93@gmail.com>
2025-03-02 21:25:35 +0100

    FinalProyectofin...

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 
+
 export default function ItemCount({ stock, initial, onAdd }) {
   const [count, setCount] = useState(initial);
 
